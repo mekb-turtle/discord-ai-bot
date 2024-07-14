60be7bcebe4003da9c0d8d0b55dc283bd89b06ce
Samuel Jędrzejewski <samijedrzejewski@gmail.com>
2024-07-14 23:52:08 +0200

    Added stable diffusion command

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,27 @@
-import { Client, Events, GatewayIntentBits, MessageType, Partials } from "discord.js";
+import {
+	Client,
+	Events,
+	GatewayIntentBits,
+	MessageType,
+	Partials,
+	REST,
+	Routes,
+} from "discord.js";
 import { Logger, LogLevel } from "meklog";
 import dotenv from "dotenv";
 import axios from "axios";
+import commands from "./commands/commands.js";
 
 dotenv.config();
 
 const model = process.env.MODEL;
-const servers = process.env.OLLAMA.split(",").map(url => ({ url: new URL(url), available: true }));
+const servers = process.env.OLLAMA.split(",").map((url) => ({
+	url: new URL(url),
+	available: true,
+}));
+const stableDiffusionServers = process.env.STABLE_DIFFUSION.split(",").map(
+	(url) => ({ url: new URL(url), available: true })
+);
 const channels = process.env.CHANNELS.split(",");
 
 if (servers.length == 0) {
@@ -14,7 +29,7 @@ if (servers.length == 0) {
 }
 
 let log;
-process.on("message", data => {
+process.on("message", (data) => {
 	if (data.shardID) client.shardID = data.shardID;
 	if (data.logger) log = new Logger(data.logger);
 });
@@ -40,9 +55,9 @@ function shuffleArray(array) {
 }
 
 async function makeRequest(path, method, data) {
-	while (servers.filter(server => server.available).length == 0) {
+	while (servers.filter((server) => server.available).length == 0) {
 		// wait until a server is available
-		await new Promise(res => setTimeout(res, 1000));
+		await new Promise((res) => setTimeout(res, 1000));
 	}
 
 	let error = null;
@@ -65,13 +80,61 @@ async function makeRequest(path, method, data) {
 			url.pathname += path;
 			log(LogLevel.Debug, `Making request to ${url}`);
 			const result = await axios({
-				method, url, data,
-				responseType: "text"
+				method,
+				url,
+				data,
+				responseType: "text",
 			});
 			servers[i].available = true;
 			return result.data;
 		} catch (err) {
 			servers[i].available = true;
+			error = err;
+			logError(error);
+		}
+	}
+	if (!error) {
+		throw new Error("No servers available");
+	}
+	throw error;
+}
+
+async function makeStableDiffusionRequest(path, method, data) {
+	while (
+		stableDiffusionServers.filter((server) => server.available).length == 0
+	) {
+		// wait until a server is available
+		await new Promise((res) => setTimeout(res, 1000));
+	}
+
+	let error = null;
+	// randomly loop through the servers available, don't shuffle the actual array because we want to be notified of any updates
+	let order = new Array(stableDiffusionServers.length).fill().map((_, i) => i);
+	if (randomServer) order = shuffleArray(order);
+	for (const j in order) {
+		if (!order.hasOwnProperty(j)) continue;
+		const i = order[j];
+		// try one until it succeeds
+		try {
+			// make a request to stable diffusion
+			if (!stableDiffusionServers[i].available) continue;
+			const url = new URL(stableDiffusionServers[i].url); // don't modify the original URL
+
+			stableDiffusionServers[i].available = false;
+
+			if (path.startsWith("/")) path = path.substring(1);
+			if (!url.pathname.endsWith("/")) url.pathname += "/"; // safety
+			url.pathname += path;
+			log(LogLevel.Debug, `Making stable diffusion request to ${url}`);
+			const result = await axios({
+				method,
+				url,
+				data,
+			});
+			stableDiffusionServers[i].available = true;
+			return result.data;
+		} catch (err) {
+			stableDiffusionServers[i].available = true;
 			error = err;
 			logError(error);
 		}
@@ -88,17 +151,22 @@ const client = new Client({
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.GuildMembers,
 		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.MessageContent
+		GatewayIntentBits.MessageContent,
 	],
 	allowedMentions: { users: [], roles: [], repliedUser: false },
-	partials: [
-		Partials.Channel
-	]
+	partials: [Partials.Channel],
 });
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 client.once(Events.ClientReady, async () => {
 	await client.guilds.fetch();
 	client.user.setPresence({ activities: [], status: "online" });
+	await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+		body: commands,
+	});
+
+	log(LogLevel.Info, "Successfully reloaded application slash (/) commands.");
 });
 
 const messages = {};
@@ -107,7 +175,8 @@ const messages = {};
 function splitText(str, length) {
 	// trim matches different characters to \s
 	str = str
-		.replace(/\r\n/g, "\n").replace(/\r/g, "\n")
+		.replace(/\r\n/g, "\n")
+		.replace(/\r/g, "\n")
 		.replace(/^\s+|\s+$/g, "");
 	const segments = [];
 	let segment = "";
@@ -130,7 +199,10 @@ function splitText(str, length) {
 				// append up all but last paragraph
 				const beforeParagraph = segment.match(/^.*\n/s);
 				if (beforeParagraph != null) {
-					const lastParagraph = segment.substring(beforeParagraph[0].length, segment.length);
+					const lastParagraph = segment.substring(
+						beforeParagraph[0].length,
+						segment.length
+					);
 					segment = beforeParagraph[0];
 					appendSegment();
 					segment = lastParagraph;
@@ -160,31 +232,41 @@ function getBoolean(str) {
 }
 
 function parseJSONMessage(str) {
-	return str.split(/[\r\n]+/g).map(function(line) {
-		const result = JSON.parse(`"${line}"`);
-		if (typeof result !== "string") throw new "Invalid syntax in .env file";
-		return result;
-	}).join("\n");
+	return str
+		.split(/[\r\n]+/g)
+		.map(function (line) {
+			const result = JSON.parse(`"${line}"`);
+			if (typeof result !== "string") throw new "Invalid syntax in .env file"();
+			return result;
+		})
+		.join("\n");
 }
 
 function parseEnvString(str) {
-	return typeof str === "string" ?
-		parseJSONMessage(str).replace(/<date>/gi, new Date().toUTCString()) : null;
+	return typeof str === "string"
+		? parseJSONMessage(str).replace(/<date>/gi, new Date().toUTCString())
+		: null;
 }
 
 const customSystemMessage = parseEnvString(process.env.SYSTEM);
-const useCustomSystemMessage = getBoolean(process.env.USE_SYSTEM) && !!customSystemMessage;
+const useCustomSystemMessage =
+	getBoolean(process.env.USE_SYSTEM) && !!customSystemMessage;
 const useModelSystemMessage = getBoolean(process.env.USE_MODEL_SYSTEM);
-const showStartOfConversation = getBoolean(process.env.SHOW_START_OF_CONVERSATION);
+const showStartOfConversation = getBoolean(
+	process.env.SHOW_START_OF_CONVERSATION
+);
 const randomServer = getBoolean(process.env.RANDOM_SERVER);
 let modelInfo = null;
 const initialPrompt = parseEnvString(process.env.INITIAL_PROMPT);
-const useInitialPrompt = getBoolean(process.env.USE_INITIAL_PROMPT) && !!initialPrompt;
+const useInitialPrompt =
+	getBoolean(process.env.USE_INITIAL_PROMPT) && !!initialPrompt;
 
 const requiresMention = getBoolean(process.env.REQUIRES_MENTION);
 
 async function replySplitMessage(replyMessage, content) {
-	const responseMessages = splitText(content, 2000).map(content => ({ content }));
+	const responseMessages = splitText(content, 2000).map((content) => ({
+		content,
+	}));
 
 	const replyMessages = [];
 	for (let i = 0; i < responseMessages.length; ++i) {
@@ -197,7 +279,7 @@ async function replySplitMessage(replyMessage, content) {
 	return replyMessages;
 }
 
-client.on(Events.MessageCreate, async message => {
+client.on(Events.MessageCreate, async (message) => {
 	let typing = false;
 	try {
 		await message.fetch();
@@ -211,7 +293,10 @@ client.on(Events.MessageCreate, async message => {
 		if (message.author.bot || message.author.id == client.user.id) return;
 
 		const botRole = message.guild?.members?.me?.roles?.botRole;
-		const myMention = new RegExp(`<@((!?${client.user.id}${botRole ? `)|(&${botRole.id}` : ""}))>`, "g"); // RegExp to match a mention for the bot
+		const myMention = new RegExp(
+			`<@((!?${client.user.id}${botRole ? `)|(&${botRole.id}` : ""}))>`,
+			"g"
+		); // RegExp to match a mention for the bot
 
 		if (typeof message.content !== "string" || message.content.length == 0) {
 			return;
@@ -230,11 +315,12 @@ client.on(Events.MessageCreate, async message => {
 
 		// fetch info about the model like the template and system message
 		if (modelInfo == null) {
-			modelInfo = (await makeRequest("/api/show", "post", {
-				name: model
-			}));
+			modelInfo = await makeRequest("/api/show", "post", {
+				name: model,
+			});
 			if (typeof modelInfo === "string") modelInfo = JSON.parse(modelInfo);
-			if (typeof modelInfo !== "object") throw "failed to fetch model information";
+			if (typeof modelInfo !== "object")
+				throw "failed to fetch model information";
 		}
 
 		const systemMessages = [];
@@ -252,7 +338,8 @@ client.on(Events.MessageCreate, async message => {
 
 		// deal with commands first before passing to LLM
 		let userInput = message.content
-			.replace(new RegExp("^\s*" + myMention.source, ""), "").trim();
+			.replace(new RegExp("^s*" + myMention.source, ""), "")
+			.trim();
 
 		// may change this to slash commands in the future
 		// i'm using regular text commands currently because the bot interacts with text content anyway
@@ -270,7 +357,9 @@ client.on(Events.MessageCreate, async message => {
 						delete messages[channelID];
 
 						if (cleared > 0) {
-							await message.reply({ content: `Cleared conversation of ${cleared} messages` });
+							await message.reply({
+								content: `Cleared conversation of ${cleared} messages`,
+							});
 							break;
 						}
 					}
@@ -279,15 +368,21 @@ client.on(Events.MessageCreate, async message => {
 				case "help":
 				case "?":
 				case "h":
-					await message.reply({ content: "Commands:\n- `.reset` `.clear`\n- `.help` `.?` `.h`\n- `.ping`\n- `.model`\n- `.system`" });
+					await message.reply({
+						content:
+							"Commands:\n- `.reset` `.clear`\n- `.help` `.?` `.h`\n- `.ping`\n- `.model`\n- `.system`\nSlash commands:\n- `/text2img`",
+					});
 					break;
 				case "model":
 					await message.reply({
-						content: `Current model: ${model}`
+						content: `Current model: ${model}`,
 					});
 					break;
 				case "system":
-					await replySplitMessage(message, `System message:\n\n${systemMessage}`);
+					await replySplitMessage(
+						message,
+						`System message:\n\n${systemMessage}`
+					);
 					break;
 				case "ping":
 					// get ms difference
@@ -300,13 +395,21 @@ client.on(Events.MessageCreate, async message => {
 				case "":
 					break;
 				default:
-					await message.reply({ content: "Unknown command, type `.help` for a list of commands" });
+					await message.reply({
+						content: "Unknown command, type `.help` for a list of commands",
+					});
 					break;
 			}
 			return;
 		}
 
-		if (message.type == MessageType.Default && (requiresMention && message.guild && !message.content.match(myMention))) return;
+		if (
+			message.type == MessageType.Default &&
+			requiresMention &&
+			message.guild &&
+			!message.content.match(myMention)
+		)
+			return;
 
 		if (message.guild) {
 			await message.guild.channels.fetch();
@@ -343,7 +446,12 @@ client.on(Events.MessageCreate, async message => {
 		}
 
 		// log user's message
-		log(LogLevel.Debug, `${message.guild ? `#${message.channel.name}` : "DMs"} - ${message.author.username}: ${userInput}`);
+		log(
+			LogLevel.Debug,
+			`${message.guild ? `#${message.channel.name}` : "DMs"} - ${
+				message.author.username
+			}: ${userInput}`
+		);
 
 		// start typing
 		typing = true;
@@ -372,21 +480,26 @@ client.on(Events.MessageCreate, async message => {
 			}
 
 			// make request to model
-			response = (await makeRequest("/api/generate", "post", {
+			response = await makeRequest("/api/generate", "post", {
 				model: model,
 				prompt: userInput,
 				system: systemMessage,
-				context
-			}));
+				context,
+			});
 
 			if (typeof response != "string") {
 				log(LogLevel.Debug, response);
-				throw new TypeError("response is not a string, this may be an error with ollama");
+				throw new TypeError(
+					"response is not a string, this may be an error with ollama"
+				);
 			}
 
-			response = response.split("\n").filter(e => !!e).map(e => {
-				return JSON.parse(e);
-			});
+			response = response
+				.split("\n")
+				.filter((e) => !!e)
+				.map((e) => {
+					return JSON.parse(e);
+				});
 		} catch (error) {
 			if (typingInterval != null) {
 				clearInterval(typingInterval);
@@ -400,21 +513,29 @@ client.on(Events.MessageCreate, async message => {
 		}
 		typingInterval = null;
 
-		let responseText = response.map(e => e.response).filter(e => e != null).join("").trim();
+		let responseText = response
+			.map((e) => e.response)
+			.filter((e) => e != null)
+			.join("")
+			.trim();
 		if (responseText.length == 0) {
 			responseText = "(No response)";
 		}
 
 		log(LogLevel.Debug, `Response: ${responseText}`);
 
-		const prefix = showStartOfConversation && messages[channelID].amount == 0 ?
-			"> This is the beginning of the conversation, type `.help` for help.\n\n" : "";
+		const prefix =
+			showStartOfConversation && messages[channelID].amount == 0
+				? "> This is the beginning of the conversation, type `.help` for help.\n\n"
+				: "";
 
 		// reply (will automatically stop typing)
-		const replyMessageIDs = (await replySplitMessage(message, `${prefix}${responseText}`)).map(msg => msg.id);
+		const replyMessageIDs = (
+			await replySplitMessage(message, `${prefix}${responseText}`)
+		).map((msg) => msg.id);
 
 		// add response to conversation
-		context = response.filter(e => e.done && e.context)[0].context;
+		context = response.filter((e) => e.done && e.context)[0].context;
 		for (let i = 0; i < replyMessageIDs.length; ++i) {
 			messages[channelID][replyMessageIDs[i]] = context;
 		}
@@ -428,6 +549,54 @@ client.on(Events.MessageCreate, async message => {
 			} catch (ignored) {}
 		}
 		logError(error);
+	}
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName, options } = interaction;
+	const prompt = options.getString("prompt");
+	const width = options.getNumber("width") || 256;
+	const height = options.getNumber("height") || 256;
+	const steps = options.getNumber("steps") || 10;
+	const batch_count = options.getNumber("batch_count") || 1;
+	const batch_size = options.getNumber("batch_size") || 1;
+	const enhance_prompt =
+		options.getBoolean("enhance_prompt") || true ? "yes" : "no";
+
+	switch (commandName) {
+		case "text2img":
+			try {
+				await interaction.deferReply();
+				let stableDiffusionResponse = await makeStableDiffusionRequest(
+					"/sdapi/v1/txt2img",
+					"post",
+					{
+						prompt,
+						width,
+						height,
+						steps,
+						num_inference_steps: steps,
+						batch_count,
+						batch_size,
+						enhance_prompt,
+					}
+				);
+				const images = stableDiffusionResponse.images.map((image) =>
+					Buffer.from(image, "base64")
+				);
+				await interaction.editReply({
+					content: `Here are images from prompt \`${prompt}\``,
+					files: images,
+				});
+			} catch (error) {
+				logError(error);
+				await interaction.editReply({
+					content: "Error, please check the console",
+				});
+			}
+			break;
 	}
 });
 
